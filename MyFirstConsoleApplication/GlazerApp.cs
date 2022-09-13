142c3efb03966306744bf1f07509b5b0a8e409af
chasemoses <43392487+chasemoses@users.noreply.github.com>
2022-09-13 02:16:52 -0600

    Add project files.

---
+++ MyFirstConsoleApplication/GlazerApp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApplication
+{
+    internal static class GlazerApp
+    {
+        private static double Width { get; set; }
+        private static double Height { get; set; }
+        private static double WoodLength { get; set; }
+        private static double GlassArea { get; set; }
+        private static string WidthString { get; set; }
+        private static string HeightString { get; set; }
+
+
+        public static void RunExample()
+        {
+            // Ask for WidthString Value
+            Console.WriteLine("Please enter the width: ");
+            WidthString = Console.ReadLine();
+
+            // Ask for HidthString Value
+            Console.WriteLine("Please enter the height: ");
+            HeightString = Console.ReadLine();
+
+
+            // Parse into respective values
+            Width = double.Parse(WidthString);
+
+            Height = double.Parse(HeightString);
+
+
+            // Do Calculations
+            WoodLength = 2 * (Width + Height) * 3.25;
+
+            GlassArea = 2 * (Width * Height);
+
+
+            // Display to user
+            Console.WriteLine($"The length of the wood is {WoodLength} feet");
+            Console.WriteLine($"The area of the glass is {GlassArea} square metres");
+        }
+    }
+}
---
+++ MyFirstConsoleApplication/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApplication
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            DateTime date = DateTime.Now;
+
+            GetUserNameAndLocation();
+            Console.WriteLine();
+
+            ChristmasCountdown(date);
+            Console.WriteLine();
+
+            GlazerApp.RunExample();
+            Console.ReadKey();
+        }
+
+
+        private static void GetUserNameAndLocation()
+        {
+            Person person = new Person();
+
+            // Prompt user for name and store it in the "Name" field.
+            Console.WriteLine("What is your name?");
+            person.Name = Console.ReadLine();
+
+            // Do Response
+            Console.WriteLine($"Hi, {person.Name}! Where are you from?");
+
+            // Record response in Location
+            person.Location = Console.ReadLine();
+
+            // Third Response
+            Console.WriteLine($"I have never been to {person.Location}. I bet it is nice.");
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+
+        }
+
+        private static void ChristmasCountdown(DateTime date)
+        {
+            // Display current date.
+            Console.WriteLine($"Today's date is: {date.ToShortDateString()}");
+
+            // Calculate number of days until Christmas
+            DateTime christmas = new DateTime(2022, 12, 25);
+
+            TimeSpan daysUntilChristmas = christmas - date;
+
+            // Display days until Christmas to user.
+            Console.WriteLine($"There are {daysUntilChristmas.Days} days until Christmas!");
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
